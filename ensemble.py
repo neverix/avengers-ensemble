c345a0da62aa943dc9a58deb759591a5b213711c
neverix <nev@neverix.io>
2021-02-14 11:58:50 +0300

    Word and more golden

--- ensemble.py
+++ ensemble.py
@@ -91,6 +91,7 @@ models = ("xlm/anli", "xlm/anli-terra", "xlm/anli-all", "xlm/anli-all-x", "xlm/a
           "zero-norm/super-rcb", "silver/silver", "golden/1", "golden/2", "golden/3", "golden/4",
           "zero-norm/super-qa", "golden/danetqa", "golden/danetqa-better", "zero-norm/super-proc",
           "golden/danetqa-5000", "platinum/1", "zero56/zero", "platinum/1-fp", "platinum/1r", "platinum/1rs",
+          "gpt/medium-mrc", "golden/mix", "word/word",
           "zero/zero", "zero-alt/zero", "zero-alt/zero83", "zero-norm/zero", "mbert/mbert")[:-1]
 datasets = {
     data.read_rwsd: (process_rwsd, "RWSD", "acc"),
